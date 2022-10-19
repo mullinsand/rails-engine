e6ac102b21078b7c97e16b2c3cf3a61ae5cb1355
Andrew Mullins <mullinsand15@gmail.com>
2022-10-19 09:55:57 -0600

    dryed up exception handler with helper method

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,29 +6,32 @@ module ExceptionHandler
       render json: { errors: error.message }, status: :not_found
     end
 
-    # rescue_from ActiveRecord::RecordInvalid do |error|
-    #   render json: { errors: error.message }, status: :unprocessable_entity
-    # end
+    rescue_from ActiveRecord::RecordInvalid do |error|
+      render json: { errors: error.message }, status: :unprocessable_entity
+    end
+  end
 
+  def render_error(error, status)
+    render json: { errors: error }, status: status
   end
 
   def negative_number_error
-    render json: { errors: 'Prices must be greater than or equal to zero' }, status: 400
+    render_error('Prices must be greater than or equal to zero', 400)
   end
 
   def name_and_price_error
-    render json: { errors: 'Name and price cannot be used on the same request' }, status: 400
+    render_error('Name and price cannot be used on the same request', 400)
   end
 
   def empty_params_error
-    render json: { errors: 'No params listed in search' }, status: 400
-  end
-
-  def no_search_results
-    render json: { data: { message: 'No results matched your search'}}, status: 200
+    render_error('No params listed in search', 400)
   end
 
   def min_greater_than_max
-    render json: { errors: 'Price minimum must be greater than maximum' }, status: 400
+    render_error('Price minimum must be greater than maximum', 400)
+  end
+
+  def no_search_results
+    render json: { data: { message: 'No results matched your search' } }, status: 200
   end
 end
