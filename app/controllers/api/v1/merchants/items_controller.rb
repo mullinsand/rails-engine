86ccbbf4f0731549f8ed2d402cc7c067bded0bc6
Andrew Mullins <mullinsand15@gmail.com>
2022-10-19 06:47:01 -0600

    move merchant find and find all methods to search controller

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Merchants::ItemsController < ApplicationController
   include ExceptionHandler
   def index
     Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(::Item.where(merchant_id: params[:merchant_id]))
+    render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
   end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,34 +1,10 @@
 class Api::V1::MerchantsController < ApplicationController
   include ExceptionHandler
   def index
-    render json: MerchantSerializer.new(::Merchant.all)
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: MerchantSerializer.new(::Merchant.find(params[:id]))
-  end
-
-  def find_all
-    if params_present?(params[:name])
-      merchants = ::Merchant.find_by_name(params[:name], 'all')
-      render json: MerchantSerializer.new(merchants)
-    else
-      empty_params_error
-    end
-  end
-
-  def find
-    if params_present?(params[:name])
-      merchant = ::Merchant.find_by_name(params[:name])
-      merchant ? (render json: MerchantSerializer.new(merchant)) : no_search_results
-    else
-      empty_params_error
-    end
-  end
-
-  private
-
-  def params_present?(param)
-    ![nil, ''].include?(param)
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
