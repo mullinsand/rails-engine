45e030a3e0be433670cd28af327e03c7b80a3a58
Andrew Mullins <mullinsand15@gmail.com>
2022-10-19 06:37:08 -0600

    moves find and find all methods to search controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/merchants/find', controller: "merchants", action: "find", as: :merchant_find
-      get '/merchants/find_all', controller: 'merchants', action: 'find_all', as: :merchants_find
+      get '/merchants/find', controller: "merchants/search", action: "find", as: :merchant_find
+      get '/merchants/find_all', controller: 'merchants/search', action: 'find_all', as: :merchants_find
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index], controller: 'merchant/items'
+        resources :items, only: [:index], controller: 'merchants/items'
       end
-      get '/items/find', controller: "items", action: "find", as: :item_find
-      get '/items/find_all', controller: 'items', action: 'find_all', as: :items_find
+      get '/items/find', controller: "items/search", action: "find", as: :item_find
+      get '/items/find_all', controller: 'items/search', action: 'find_all', as: :items_find
       resources :items do
-        resource :merchant, only: [:show], controller: 'item/merchant'
+        resource :merchant, only: [:show], controller: 'items/merchant'
       end
     end
   end
